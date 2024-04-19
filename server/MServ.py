1639499ac673529c34b3b1ac6858ea2442e6b5d0
【﻿Ｋｉｋｏ】 <53761278+BadKiko@users.noreply.github.com>
2024-04-20 02:43:21 +0300

    Update MServ.py

--- server/MServ.py
+++ server/MServ.py
@@ -130,7 +130,7 @@ def localServer(logLevel: str = "critical"):
         logger.error(f"[Voice Changer] Web Server Launch Exception, {e}")
 
 
-if __name__ == "MMVCServerSIO":
+if __name__ == "MServ":
     mp.freeze_support()
 
     voiceChangerManager = VoiceChangerManager.get_instance(voiceChangerParams)
